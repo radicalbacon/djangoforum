2e6d6298411f19bf01787bd982b1769d0b734668
S.Lee radicalbacon <s.m.lee.data@gmail.com>
2022-08-25 16:14:18 -0400

    message

---
+++ posts/views.py
@@ -0,0 +1,49 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+# Create your views here.
+
+
+def index(request):
+    # If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        # If the form is valid
+        if form.is_valid():
+            # Yes, Save
+            form.save()
+            print("Hello its valid")
+
+            # Redirect to Home
+            return HttpResponseRedirect('/')
+
+        else:
+            # No,Show Error
+            print("its not valid")
+            return HttpResponseRedirect(form.errors.as_json())
+    # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_at')[:20]
+    form = PostForm()
+    return render(request, 'posts.html', {'posts': posts})
+
+
+def delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+
+def edit(request, id):
+    if request.method == "GET":
+        posts = Post.objects.get(id=id)
+        return render(request, "edit.html", {"posts": posts})
+    if request.method == "POST":
+        editposts = Post.objects.get(id=id)
+        form = PostForm(request.POST, request.FILES, instance=editposts)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponse("not valid")
